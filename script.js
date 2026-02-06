528c369699f99347746c782270aa246e577631d1
initnegi <chaitanya.negi.nc@gmail.com>
2026-02-07 02:33:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-/* MODE TOGGLE */
-const modeBtn = document.getElementById("modeToggle");
-
-modeBtn.onclick = () => {
-  document.body.classList.toggle("clean");
-  modeBtn.textContent =
-    document.body.classList.contains("clean")
-      ? "CYBER MODE"
-      : "FAANG MODE";
-};
-
-/* THEME SWITCH */
-document.getElementById("themeSelect").onchange = (e) => {
+// Theme switcher (dark only)
+document.getElementById("themeSelect").addEventListener("change", (e) => {
   document.body.dataset.theme = e.target.value;
-};
+});
